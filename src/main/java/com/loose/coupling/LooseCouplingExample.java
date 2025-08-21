374586503f2e92f571730c6fa694988f0223942c
AshishPradhan257 <ashish257pradhan@gmail.com>
2025-08-22 01:45:27 +0530

    bean and dependency injection

--- src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -4,6 +4,8 @@ package com.loose.coupling;
 public class LooseCouplingExample {
 
     public static void main(String[] args){
+
+        //by spring framework this can be done directly, without providing the dependency!
         UserDataProvider databaseProvider = new UserDatabaseProvider();
         UserManager userManagerWithDB = new UserManager(databaseProvider);
         System.out.println(userManagerWithDB.getUserInfo());
